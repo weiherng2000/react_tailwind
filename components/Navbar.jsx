e8402a05ab977dbdde477ac08b198466266da1df
Tan wei herng <weiherng2000@gmail.com>
2023-07-16 17:49:15 +0800

    yes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,12 +12,33 @@ import {navVariants} from '../utils/motion'; /*utils motion contains some specif
 /*Variants are pre-defined visual states that a component can be in. By giving a component 
 and its children variants with matching names, whole React trees can be animated by changing a single prop.*/
 const Navbar = () => (
+  /*animates the navbar from hidden state to show state */
+  /*self closing div are like empty div blocks*/
+  /*inset-0 fills the entire parent*/
   <motion.nav
    variants = {navVariants}
    initial = "hidden"
    whileInView="show"
    className = {`${styles.xPaddings} py-8 relative`}>
-    navbar
+  
+  <div className = "absolute w-[50%] inset-0 gradient-01"/>
+  <div className = {`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+    <img
+     src = "./search.svg"
+     alt = "search"
+     className = "w-[24px] h-[24px] object-contain"
+    />
+    <h2 className ="font-extrabold text-[24px]
+    leading-[30px] text-white">
+      METAVERSE
+    </h2>
+    <img 
+    src = "menu.svg"
+    alt = "menu"
+    className = "w-[24px] h-[24px] object-contain"/>
+    
+  </div>
+
   </motion.nav>
 );
 
