5f6d6ca708024b837a62a091ea30af2f3c32db77
Tan wei herng <weiherng2000@gmail.com>
2023-07-16 16:21:52 +0800

    test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,23 @@
-'use client';
+'use client';/* allows us to use usestate and other hooks from react*/
+/*client is to ensure this file on client side*/
 
+import {motion} from 'framer-motion';
+
+import styles from '../styles';/*styles is index.js but we export it as styles component*/
+import {navVariants} from '../utils/motion'; /*utils motion contains some specific code for animation*/
+
+/* motion is an animation component and to use it we usually call motion.div /motion.nav/motion.{anyelement}*/
+
+/*Variants are pre-defined visual states that a component can be in. By giving a component 
+and its children variants with matching names, whole React trees can be animated by changing a single prop.*/
 const Navbar = () => (
-  <nav>
+  <motion.nav
+   variants = {navVariants}
+   initial = "hidden"
+   whileInView="show"
+   className = {`${styles.xPaddings} py-8 relative`}>
     navbar
-  </nav>
+  </motion.nav>
 );
 
 export default Navbar;
