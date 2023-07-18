7c737a39eb94e02517b37ed35cabb7f05f064755
Tan wei herng <weiherng2000@gmail.com>
2023-07-18 17:52:38 +0800

    some touches

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,37 @@
 'use client';
 
+import {motion} from 'framer-motion';
+import {InsightCard,TitleText,TypingText } from '../components';
+import styles from '../styles';/*styles is index.js but we export it as styles component*/
+import {staggerContainer} from '../utils/motion'; /*utils motion contains some specific code for animation*/
+import{insights} from '../constants';
+
+
+
 const Insights = () => (
-  <section>
-    Insights section
+  <section className = {`${styles.paddings} relative z-10`}>
+    <motion.div
+    variants={staggerContainer}
+    inital = "hidden"
+    whileInView= "show"
+    viewport = {{once: 'false' ,amount: 0.25}}
+    className = {`${styles.innerWidth} mx-auto flex
+   flex-col `}>
+
+    <TypingText title = "| Insight"
+    textStyles= "text-center"/>
+
+    <TitleText title = "Insight about Metaverse"
+    textStyles="text-center"/>
+
+    <div className = "mt-[50px] flex flex-col gap-[30px]">
+        {insights.map((insight,index) => (
+          <InsightCard key = {`insight-${index}`}
+          {...insight} index = {index+1} />
+        ))}
+    </div>
+
+   </motion.div>
   </section>
 );
 
